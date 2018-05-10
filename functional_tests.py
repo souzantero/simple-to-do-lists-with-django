75ed5431890595ee2f9bcf1a4a41717929c155ce
Felipe Antero <antero@Felipes-MacBook-Pro.local>
2018-05-10 15:21:43 -0300

    Changes to be committed: 	modified:   functional_tests.py

--- functional_tests.py
+++ functional_tests.py
@@ -7,8 +7,13 @@ class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
-    def setDown(self):
+    def tearDown(self):
         self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith ouvir falar de uma nova aplicação online interessante para
@@ -34,9 +39,7 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers" como um item em uma lista de tarefas
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # Ainda continua havendo uma caixa de texto convidado-a a acresentar outro item.
         # Ela insere "Use peacock feathers to make a fly"
@@ -46,10 +49,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # A página é atualizada novamente e agora mostra os dois itens em sua lista
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         self.fail('Finish the test!')
 
