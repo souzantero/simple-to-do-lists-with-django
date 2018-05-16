7b8e0110fb83c52d345ee86bc0547959cfbba4ac
Felipe Antero <antero@Felipes-MacBook-Pro.local>
2018-05-16 11:43:54 -0300

    List model added on project

--- lists/tests.py
+++ lists/tests.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 class HomePageTest(TestCase):
     def test_use_home_template(self):
@@ -14,8 +14,10 @@ class HomePageTest(TestCase):
 
 class ListViewTest(TestCase):
     def test_displays_all_items(self):
-        Item.objects.create(text = 'itemey 1')
-        Item.objects.create(text = 'itemey 2')
+        new_list = List.objects.create()
+
+        Item.objects.create(text = 'itemey 1', list = new_list)
+        Item.objects.create(text = 'itemey 2', list = new_list)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
@@ -45,15 +47,23 @@ class NewListTest(TestCase):
                 }),
             '/lists/the-only-list-in-the-world/')
 
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        new_list = List()
+        new_list.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = new_list
         first_item.save()
 
         second_item = Item()
         second_item.text = "Item the second"
+        second_item.list = new_list
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, new_list)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -61,4 +71,6 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, new_list)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, new_list)
--- lists/views.py
+++ lists/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from lists.models import Item
+from lists.models import Item, List
 
 def home_page(request):
     return render(request, "home.html")
 
 def new_list(request):
-    Item.objects.create(text = request.POST['item_text'])
+    Item.objects.create(
+        text = request.POST['item_text'],
+        list = List.objects.create()
+    )
+
     return redirect('/lists/the-only-list-in-the-world/')
 
 def view_list(request):
