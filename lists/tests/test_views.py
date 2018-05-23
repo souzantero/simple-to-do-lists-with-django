d226e1611a46ce99b80d6a612452a9701bd72122
Felipe Antero <antero@Felipes-MacBook-Pro.local>
2018-05-23 11:29:03 -0300

    Split out unit tests into two files

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -87,31 +87,3 @@ class NewItemTest(TestCase):
         )
 
         self.assertRedirects(response, f'/lists/{correct_list.id}/')
-
-class ListAndItemModelsTest(TestCase):
-    def test_saving_and_retrieving_items(self):
-        new_list = List()
-        new_list.save()
-
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.list = new_list
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = "Item the second"
-        second_item.list = new_list
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, new_list)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(first_saved_item.list, new_list)
-        self.assertEqual(second_saved_item.text, 'Item the second')
-        self.assertEqual(second_saved_item.list, new_list)
