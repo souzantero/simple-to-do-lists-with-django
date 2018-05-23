ac91b4ed2e99e918ab16ee922acc1c889ee00d0a
Felipe Antero <antero@Felipes-MacBook-Pro.local>
2018-05-23 11:22:37 -0300

    Added test to validate empty list items

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -29,3 +29,13 @@ class FunctionalTest(StaticLiveServerTestCase):
                 if time.time() - start_time > MAX_WAIT:
                     raise exception
                 time.sleep(0.5)
+
+    def wait_for(self, func):
+        start_time = time.time()
+        while True:
+            try:
+                return func()
+            except (AssertionError, WebDriverException) as exception:
+                if time.time() - start_time > MAX_WAIT:
+                    raise exception
+                time.sleep(0.5)
--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -5,16 +5,35 @@ class ItemValidationTest(FunctionalTest):
     def test_cannot_add_empty_list_items(self):
         # Edith acessa a página inicial e acidentalmente tenta submeter
         # um item vazio na lista. Ela tecla Enter na caixa de entrada vazia
+        self.browser.get(self.live_server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         # A página inicial é atualizada e há uma mensagem de erro informando
         # que itens da lista não podem estar em branco
+        self.wait_for(lambda:
+            self.assertEqual(
+                self.browser.find_element_by_css_selector('.has-error').text,
+                "You can't have an empty list item"
+            )
+        )
 
         # Ela tenta novamente com um texto para o item, e isso agora funciona
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
 
         # De forma perversa, ela agora decide submeter um segundo item em branco na lista
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         # Ela recebe um aviso semelhante na página da lista
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "You can't have an empty list item"
+        ))
 
-        # E ela pode corrigir isso preenchendo o item com um texto 
-        self.fail('write me!')
+        # E ela pode corrigir isso preenchendo o item com um texto
+        self.browser.find_element_by_id('id_new_item').send_keys('Make tea')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+        self.wait_for_row_in_list_table('2: Make tea')
 
