eb05e5ed30c1dbbf88135c8da5341b28c7199f9a
Felipe Antero <antero@Felipes-MacBook-Pro.local>
2018-05-23 12:10:58 -0300

    Adjust new list view to do model validation

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError
 from lists.models import Item, List
 
 class ListAndItemModelsTest(TestCase):
@@ -28,3 +29,11 @@ class ListAndItemModelsTest(TestCase):
         self.assertEqual(first_saved_item.list, new_list)
         self.assertEqual(second_saved_item.text, 'Item the second')
         self.assertEqual(second_saved_item.list, new_list)
+
+    def test_cannot_save_empty_list_items(self):
+        new_list = List.objects.create()
+        item = Item(list = new_list, text = '')
+
+        with self.assertRaises(ValidationError):
+            item.save()
+            item.full_clean()
--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.core.exceptions import ValidationError
 from lists.models import Item, List
 
 def home_page(request):
@@ -7,11 +8,21 @@ def home_page(request):
 
 def new_list(request):
     created_list = List.objects.create()
-
-    Item.objects.create(
+    item = Item(
         text = request.POST['item_text'],
         list = created_list
     )
+
+    try:
+        item.full_clean()
+        item.save()
+    except ValidationError:
+        created_list.delete()
+        
+        error = "You can't have an empty list item"
+        return render(request, 'home.html', {
+            'error': error
+        })
 
     return redirect(f'/lists/{created_list.id}/')
 
